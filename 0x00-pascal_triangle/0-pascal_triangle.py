90bac2fff581cbaaea0576658c123e20d30781ff
maggie-njeri <mkiri00@gmail.com>
2024-06-26 15:53:29 +0000

    pascals

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#Pascal's Triangle
+
+
+def pascal_triangle(n):
+    #A function that returns a list of integers
+    if n <= 0:
+        return []
+
+    # Initializes pascal's triangle with the first row
+    triangle = [[1]]
+
+    # Generates other rows
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
